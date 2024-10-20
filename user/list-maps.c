3a771a7934231f9767a3a72e465e7798245fc37b
7244 <65ad4d@gmail.com>
2024-10-20 13:21:36 +0000

    get_prog_id_by_map_id added to list-maps.c

--- user/list-maps.c
+++ user/list-maps.c
@@ -11,26 +11,82 @@ void print_hexstring(const void *ptr, uintptr_t size){
   }
 }
 
+/* returns 0 when prog is not found */
+int get_prog_id_by_map_id(__u32 map_id){
+  struct bpf_prog_info prog_info;
+  __u32 prog_info_len = sizeof(prog_info);
+
+  __u32 prog_id = 0;
+  while(bpf_prog_get_next_id(prog_id, &prog_id) == 0){
+
+    int prog_fd = bpf_prog_get_fd_by_id(prog_id);
+    if(prog_fd < 0){
+      perror("");
+      exit(1);
+    }
+
+    memset(&prog_info, 0, sizeof(prog_info));
+    if(bpf_prog_get_info_by_fd(prog_fd, &prog_info, &prog_info_len)){
+      perror("");
+      exit(1);
+    }
+
+    __u32 num_maps = prog_info.nr_map_ids;
+    memset(&prog_info, 0, sizeof(prog_info));
+    prog_info.nr_map_ids = num_maps;
+    prog_info.map_ids = (uintptr_t)malloc(num_maps * sizeof(__u32));
+    memset((void *)prog_info.map_ids, 0, num_maps * sizeof(__u32));
+
+    if(bpf_prog_get_info_by_fd(prog_fd, &prog_info, &prog_info_len)){
+      perror("");
+      exit(1);
+    }
+
+    bool found = false;
+
+    for(__u32 imap = 0; imap < prog_info.nr_map_ids; imap++){
+      if(((__u32 *)prog_info.map_ids)[imap] == map_id){
+        found = true;
+        break;
+      }
+    }
+
+    free((void *)prog_info.map_ids);
+
+    if(close(prog_fd)){
+      perror("");
+      exit(1);
+    }
+
+    if(found){
+      return prog_id;
+    }
+  }
+
+  return 0;
+}
+
 int main(){
   int err;
 
-  __u32 start_id = 0;
-  while(bpf_map_get_next_id(start_id, &start_id) == 0){
+  __u32 map_id = 0;
+  while(bpf_map_get_next_id(map_id, &map_id) == 0){
 
-    int map_fd = bpf_map_get_fd_by_id(start_id);
+    int map_fd = bpf_map_get_fd_by_id(map_id);
     if(map_fd < 0){
       perror("");
     }
 
     struct bpf_map_info info;
+    memset(&info, 0, sizeof(info));
     __u32 info_len = sizeof(struct bpf_map_info);
     err = bpf_map_get_info_by_fd(map_fd, &info, &info_len);
     if(err){
       perror("");
-      return 0;
+      return 1;
     }
 
-    printf("map info:\n");
+    printf("map info (%d:%s):\n", get_prog_id_by_map_id(map_id), info.name);
     printf(" type: %u\n", info.type);
     printf(" id: %u\n", info.id);
     printf(" key_size: %u\n", info.key_size);
